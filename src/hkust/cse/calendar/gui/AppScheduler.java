f78327a1e20441e44762e24bb445fef412c7200e
stwongaf <stwongaf@csl1wk11.win2k.cs.ust.hk>
2014-10-20 18:50:48 +0800

    modified

--- src/hkust/cse/calendar/gui/AppScheduler.java
+++ src/hkust/cse/calendar/gui/AppScheduler.java
@@ -356,23 +356,18 @@ public class AppScheduler extends JDialog implements ActionListener,
 		Appt newAppt = new Appt();
 		newAppt.setTitle(titleField.getText());
 		newAppt.setInfo(detailArea.getText());
-		int yearF_int = Utility.getNumber(yearF.getText());
-		int monthF_int = Utility.getNumber(monthF.getText());
-		int dayF_int = Utility.getNumber(dayF.getText());
-		int sTimeH_int = Utility.getNumber(sTimeH.getText());
-		int sTimeM_int = Utility.getNumber(sTimeM.getText());
-		Timestamp apptStartTime = new Timestamp(yearF_int, monthF_int, dayF_int, sTimeH_int, sTimeM_int, 0, 0);
-		int yearL_int = Utility.getNumber(yearL.getText());
-		int monthL_int = Utility.getNumber(monthL.getText());
-		int dayL_int = Utility.getNumber(dayL.getText());
-		int eTimeH_int = Utility.getNumber(eTimeH.getText());
-		int eTimeM_int = Utility.getNumber(eTimeM.getText());
-		Timestamp apptEndTime = new Timestamp(yearL_int, monthL_int, dayL_int, eTimeH_int, eTimeM_int, 0, 0);
-		TimeSpan apptTimeSpan = new TimeSpan(apptStartTime, apptEndTime);
+		int[] validDate = getValidDate();
+		int[] validTime = getValidTimeInterval();
+		TimeSpan apptTimeSpan = new TimeSpan(CreateTimeStamp(validDate, validTime[1]), CreateTimeStamp(validDate, validTime[0]));
 		newAppt.setTimeSpan(apptTimeSpan);
-		JOptionPane.showMessageDialog(this, detailArea.getText(),
-				"Confirm Save", JOptionPane.ERROR_MESSAGE);
-		parent.controller.ManageAppt(newAppt, ApptStorageControllerImpl.NEW);
+		Appt[] retrivedAppts = parent.controller.RetrieveAppts(apptTimeSpan);
+		if((validDate!=null) && (validTime!=null) && (retrivedAppts.length==0)) {
+			parent.controller.ManageAppt(newAppt, ApptStorageControllerImpl.NEW);
+			JOptionPane.showMessageDialog(this,
+					apptTimeSpan.TimeLength(), "Check Time length",
+					JOptionPane.ERROR_MESSAGE);
+			this.setVisible(false);
+		}
 	}
 
 	private Timestamp CreateTimeStamp(int[] date, int time) {
@@ -387,6 +382,7 @@ public class AppScheduler extends JDialog implements ActionListener,
 
 	public void updateSetApp(Appt appt) {
 		// Fix Me!
+		parent.controller.ManageAppt(appt, ApptStorageControllerImpl.MODIFY);
 	}
 
 	public void componentHidden(ComponentEvent e) {
