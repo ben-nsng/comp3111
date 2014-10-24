6c77f89429483d0bcb84f697dbc88f79bf8f21ef
Ben Ng <ben.nsng@gmail.com>
2014-10-24 19:50:45 +0800

    init

---
+++ src/hkust/cse/calendar/Main/TestMain.java
@@ -0,0 +1,45 @@
+package hkust.cse.calendar.Main;
+
+import java.sql.Timestamp;
+
+import hkust.cse.calendar.apptstorage.ApptStorageControllerImpl;
+import hkust.cse.calendar.apptstorage.ApptStorageMemImpl;
+import hkust.cse.calendar.unit.Appt;
+import hkust.cse.calendar.unit.TimeSpan;
+import hkust.cse.calendar.unit.User;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		
+		TestMain.TestStorage();
+		
+	}
+	
+	public static void TestStorage() {
+		User user = new User( "noname", "nopass");
+		ApptStorageControllerImpl ctrl = new ApptStorageControllerImpl(new ApptStorageMemImpl(user));
+		
+		TimeSpan time1 = new TimeSpan(new Timestamp(2014, 12, 24, 10, 0, 0, 0), new Timestamp(2014, 12, 24, 11, 0, 0, 0));
+		TimeSpan time2 = new TimeSpan(new Timestamp(2014, 12, 24, 11, 0, 0, 0), new Timestamp(2014, 12, 24, 12, 0, 0, 0));
+		
+		//create appt 1
+		Appt appt = new Appt();
+		appt.setTitle("test");
+		appt.setTimeSpan(time1);
+		ctrl.ManageAppt(appt, ApptStorageControllerImpl.NEW);
+
+		//create appt 2
+		appt = new Appt();
+		appt.setTimeSpan(time2);
+		ctrl.ManageAppt(appt, ApptStorageControllerImpl.NEW);
+		
+		//retrieve appts
+		Appt[] appts = ctrl.RetrieveAppts(user, new TimeSpan(new Timestamp(2014, 12, 24, 9, 30, 0, 0), new Timestamp(2014, 12, 24, 12, 30, 0, 0)));
+		
+		System.out.println(appts.length);
+		
+	}
+
+	
+}
