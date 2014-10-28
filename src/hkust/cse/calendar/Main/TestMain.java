02446a8d1a93637ff33b814ffe1ad3b52f598e6a
Ben Ng <ben.nsng@gmail.com>
2014-10-28 18:09:52 +0800

    test time machine

--- src/hkust/cse/calendar/Main/TestMain.java
+++ src/hkust/cse/calendar/Main/TestMain.java
@@ -4,47 +4,104 @@ import java.sql.Timestamp;
 
 import hkust.cse.calendar.apptstorage.ApptStorageControllerImpl;
 import hkust.cse.calendar.apptstorage.ApptStorageMemImpl;
+import hkust.cse.calendar.listener.TimeMachineListener;
 import hkust.cse.calendar.unit.Appt;
+import hkust.cse.calendar.unit.TimeMachine;
 import hkust.cse.calendar.unit.TimeSpan;
 import hkust.cse.calendar.unit.User;
 
-public class TestMain {
+public class TestMain implements TimeMachineListener {
 
+	private TimeMachine machine;
+	private ApptStorageControllerImpl controller;
+	private User user;
+	
 	public static void main(String[] args) {
 		
-		TestMain.TestStorage();
-		
+		TestMain test = new TestMain();
+		test.RunTest();
 	}
 	
-	public static void TestStorage() {
-		User user = new User( "noname", "nopass");
-		ApptStorageControllerImpl ctrl = new ApptStorageControllerImpl(new ApptStorageMemImpl(user));
+	public void RunTest() {
+		this.TestStorage();
+		this.TestTimeMachine();
+	}
+	
+	public void TestStorage() {
+		user = new User( "noname", "nopass");
+		controller = new ApptStorageControllerImpl(new ApptStorageMemImpl(user));
 		
-		TimeSpan time0 = new TimeSpan(new Timestamp(2014, 12, 24, 10, 30, 0, 0), new Timestamp(2014, 12, 24, 11, 30, 0, 0));
-		TimeSpan time1 = new TimeSpan(new Timestamp(2014, 12, 24, 10, 0, 0, 0), new Timestamp(2014, 12, 24, 11, 0, 0, 0));
-		TimeSpan time2 = new TimeSpan(new Timestamp(2014, 12, 24, 11, 0, 0, 0), new Timestamp(2014, 12, 24, 12, 0, 0, 0));
+		TimeSpan time0 = new TimeSpan(new Timestamp(2014 - 1900, 11, 24, 10, 30, 0, 0), new Timestamp(2014 - 1900, 11, 24, 11, 30, 0, 0));
+		TimeSpan time1 = new TimeSpan(new Timestamp(2014 - 1900, 11, 24, 10, 0, 0, 0), new Timestamp(2014 - 1900, 11, 24, 11, 0, 0, 0));
+		TimeSpan time2 = new TimeSpan(new Timestamp(2014 - 1900, 11, 24, 11, 0, 0, 0), new Timestamp(2014 - 1900, 11, 24, 12, 0, 0, 0));
 		
 		//create appt 1
 		Appt appt = new Appt();
 		appt.setTitle("test");
 		appt.setTimeSpan(time1);
-		ctrl.ManageAppt(appt, ApptStorageControllerImpl.NEW);
+		controller.ManageAppt(appt, ApptStorageControllerImpl.NEW);
 
 		//create appt 2
 		appt = new Appt();
+		appt.setTitle("test2");
 		appt.setTimeSpan(time2);
-		ctrl.ManageAppt(appt, ApptStorageControllerImpl.NEW);
+		controller.ManageAppt(appt, ApptStorageControllerImpl.NEW);
 		
 		//retrieve appts
-		Appt[] appts = ctrl.RetrieveAppts(user, time0);
+		Appt[] appts = controller.RetrieveAppts(user, time0);
 		
-		//assert appts.length == 2;
+		assert appts.length == 2;
 		//assert appts.length != 1;
-		
 		
 	}
 	
-	public static void TestTimeMachine() {
+	public void TestTimeMachine() {
+		machine = new TimeMachine();
+		machine.addElpasedListener(this);
+		
+		machine.changeStartTime(new Timestamp(2014 - 1900, 11, 24, 9, 0, 0, 0));
+		machine.changeEndTime(new Timestamp(2014 - 1900, 11, 24, 12, 0, 0, 0));
+		machine.changeTimeDelay(30 * 60 * 1000);
+		
+		machine.start();
+		
+		while(true)
+			try {
+				Thread.sleep(300);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+
+	@Override
+	public void timeElapsed(TimeMachine sender) {
+		// TODO Auto-generated method stub
+		System.out.println("Time: " + sender.getCurrentTime());
+		
+		String info = "";
+		Timestamp next = (Timestamp)sender.getCurrentTime().clone();
+		next.setTime(sender.getCurrentTime().getTime() + sender.getTimeDelay() * 2);
+		
+		Appt[] appts = controller.RetrieveAppts(user, new TimeSpan(sender.getCurrentTime(), next));
+		for(int i = 0; i < appts.length; i++) {
+			Appt currAppt = appts[i];
+			Timestamp startTime =  currAppt.TimeSpan().StartTime();
+			if(sender.getCurrentTime().compareTo(startTime) < 0)
+				info += startTime.getHours() + ":" + startTime.getMinutes() + " - " + currAppt.getTitle() + "\n";
+			
+		}
+		
+		
+		System.out.println(info);
+		
+		System.out.println();
+	}
+
+	@Override
+	public void timeStopped(TimeMachine sender) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	
